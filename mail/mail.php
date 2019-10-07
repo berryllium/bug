7c8e06f8f8a381d687f747622d3a40e25f0c3c8c
berryllium <gorkundp@yandex.ru>
2019-10-07 22:17:01 +0300

    настроил почту me-invest

--- mail/mail.php
+++ mail/mail.php
@@ -14,58 +14,30 @@ if (isset($_POST['send_type'])) {
 	
 	if ($send_type == 'feedback') {
 		$text = "Имя клиента: " . $name
-		."<br>Телефон: " .$number
-		."<br>E-mail: " .$email
-		."<br>Отзыв: " .$feedback;
+		."\r\nТелефон: " .$number
+		."\r\nE-mail: " .$email
+		."\r\nОтзыв: " .$feedback;
 	} elseif ($send_type == 'call_us') {
 		$text = "Имя клиента: " . $name
-		."<br>Телефон: " .$number
-		."<br>E-mail: " .$email;
+		."\r\nТелефон: " .$number
+		."\r\nE-mail: " .$email;
 	} elseif ($send_type == 'subscribe') {
-		$text = "<br>E-mail: " .$email
-		."<br>Пол: " .$sex;
+		$text = "\r\nE-mail: " .$email
+		."\r\nПол: " .$sex;
 	}
 	else {
 		$text = "Имя клиента: " . $name
-		."<br>Телефон: " .$number
-		."<br>E-mail: " .$email
-		."<br>Товар: " .$good;
+		."\r\nТелефон: " .$number
+		."\r\nE-mail: " .$email
+		."\r\nТовар: " .$good;
 	} 
 		
 	$success = 'Спасибо, ' . $_POST['name'] . '! Скоро мы с Вами свяжемся!';
-    $header = 'From: freestuff47.ru@beget.com\r\n';
+    $header = 'From: me-invest@bk.ru';
 	
-// если настроена почта, то раскомментируй следующую строку, а то, что дальше закомментируй	
-//	mail('gorkundp@yandex.ru', 'Туры в Австралию', $text, $header);
+	// если настроена почта, то раскомментируй следующую строку, а то, что дальше закомментируй	
+	mail('me-invest@bk.ru', $theme, $text, $header);
+	mail('gorkundp@yandex.ru', $theme, $text, $header);
 
-		
-		$mail = new PHPMailer\PHPMailer\PHPMailer();
-		$mail->IsSMTP(); // enable SMTP
-		$mail->SMTPDebug = 0; // debugging: 1 = errors and messages, 2 = messages only
-		$mail->SMTPAuth = true; // authentication enabled
-		$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
-		$mail->Host = 'ssl://smtp.beget.com';
-		$mail->Port = 465;
-		$mail->Username = 'australia@freestuff47.ru';
-		$mail->Password = 'Placebo1';
-		$mail->IsHTML(true);
-		$mail->SetFrom('australia@freestuff47.ru');
-		$mail->Subject = $theme;
-		$mail->Body = $text;
-		$mail->AddAddress('me-invest@bk.ru');
-		if (!$mail->Send()) {
-			$result = 'Ошибка отправки формы: ' . $mail->ErrorInfo;
-			return;
-		} else {
-			$result = $success;
-			return;
-		}
-		
-	
-     
-} else {
-	
-    $result = 'Ой, что-то пошло не так. Отправка данных не выполнена, попробуйте позднее!'.$name.$email.$number.$good;
 }
-echo $result;
 ?>
